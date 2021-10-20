9915e463b346ffd7add25cb4c1f5ff0e7b576da3
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2021-10-19 23:25:44 -0400

    Programming of the DriveTrain
    Will continue work after I figure out what the heck is going on in the Axiom Code, cuz half of it looks like it isn't supposed to be there.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,5 +1,26 @@
 package frc.robot;
 
 public class RobotMap {
+    // Robot Map is a centralized location at which all the numbers (ports, set speeds, etc) for the Robot can be placed
+    // so that it can be replaced easily later if need be. 
     
+    // motor port numbers
+    // Left side motor port numbers
+    public static int leftMiddleMasterPort = 1;
+
+    public static int leftFrontFollower = 2;
+    public static int leftBackFollower = 3;
+    
+    // Right side motor port numbers
+    public static int rightMiddleMasterPort = 4;
+    
+    public static int rightFrontFollower = 5;
+    public static int rightBackFollower = 6;
+    
+    // Intake stuff
+    public static int intakePivot = 46;
+    public static int intakeLeftArm = 9;
+    public static int intakeRightArm = 10;
+    public static int lift = 11;
+
 }
--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -1,7 +1,64 @@
 package frc.robot.subsystems;
+import frc.robot.RobotMap;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class DriveBase extends SubsystemBase {
+    // A class that creates the Drivebase
+    /* Drivebase Diagram
+      ___________
+    0|-         -|0
+     | -       - |
+    0|--       --|0
+     | -       - |
+    0|-_________-|0
+    
+    */
+
+    // 
+    // Declaring the motors for the left side
+    public WPI_TalonSRX leftMiddleMaster;
+    
+    public WPI_VictorSPX leftFrontMotor;
+    public WPI_VictorSPX leftBackMotor;
+    
+    // declaring the motors for the right side
+    public WPI_TalonSRX rightMiddleMaster;
+
+    public WPI_VictorSPX rightFrontMotor;
+    public WPI_VictorSPX rightBackMotor;
+
+    public DriveBase(){
+        setName("DriveBase");
+
+        this.leftMiddleMaster = new WPI_TalonSRX(RobotMap.leftMiddleMasterPort);
+
+        this.leftFrontMotor = new WPI_VictorSPX(RobotMap.leftFrontFollower);
+        this.leftBackMotor = new WPI_VictorSPX(RobotMap.leftBackFollower);
+        
+        this.rightMiddleMaster = new WPI_TalonSRX(RobotMap.rightMiddleMasterPort);
+        
+        this.rightFrontMotor = new WPI_VictorSPX(RobotMap.rightFrontFollower);
+        this.rightBackMotor = new WPI_VictorSPX(RobotMap.rightBackFollower);
+        
+        // This is bad practice but I don't really care as long as it works right now
+        // For future reference, always initialize variables and declare an instance of the variable for this stuff like above
+        SpeedControllerGroup leftMotorGroup = new SpeedControllerGroup(leftMiddleMaster, leftFrontMotor, leftBackMotor);
+        SpeedControllerGroup rightMotorGroup = new SpeedControllerGroup(rightMiddleMaster, rightFrontMotor, rightBackMotor);
+
+        DifferentialDrive tank = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
+    }
+
+    public void setSpeed(){
+        
+    }
+
+
+
     
 }
