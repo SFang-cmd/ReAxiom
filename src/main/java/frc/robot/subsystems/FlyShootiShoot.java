ca938467cc83747d6716034286e9aed7749eda58
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2022-01-16 17:43:15 -0500

    the bread has been gotten

--- src/main/java/frc/robot/subsystems/FlyShootiShoot.java
+++ src/main/java/frc/robot/subsystems/FlyShootiShoot.java
@@ -6,5 +6,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class FlyShootiShoot {
-    
+    // For BangBangController, need velocity of current motor and the desired velocity
+    // https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/math/controller/BangBangController.html#getSetpoint()
+    // https://docs.wpilib.org/en/stable/docs/software/advanced-controls/controllers/bang-bang.html
+
+    public FlyShootiShoot(){
+
+    }
+
+    public double getVelocity(){
+        return 0.0;
+    }
 }
